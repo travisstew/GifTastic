4b093766445a4f0e3bf9fb313e233814425aa3f9
Travis <travisstew214@gmail.com>
2019-09-17 17:05:51 -0400

    added images work when being clicked on

--- assets/App.js
+++ assets/App.js
@@ -6,20 +6,22 @@
       $(document).ready(function () {
       
   
-           run();
+           addBtn();
          $('#input-btn').on("click",function(){ 
          
          var user =  $("#user-input").val();
+          
          topics.push(user);
          $("#topics").empty();
-   
-           run();
-   
+           addBtn();
+          var clear = document.querySelector("#user-input");
+          clear.value = "";
+
          });
    
    
          //function for creating topic buttons taking from the array of topics
-         function run(){
+         function addBtn(){
        
          topics.forEach(function(t,index){
              
@@ -28,17 +30,19 @@
              button.attr('id', "topic"+index);
              button.attr('class', 'topic-btn');
              button.attr('data-topic', t); 
+
              button.appendTo("#topics");
             
           });
           
          }
-   
+        var dataPicCount =0;
+     
            //click a topic button to call giphy api to display images 
            $('#topics').on('click',"button" ,function(){
             var topic = $(this).data("topic");
-           console.log('hello');
-           
+          
+
             var url = `https://api.giphy.com/v1/gifs/search?q=${topic}&api_key=Jb3ttdnYHru0VwVp3pTkk1thutShY83T&limit=10`;
    
             $.ajax({
@@ -46,46 +50,80 @@
              url: url,
              
              }).then(function(results){
-               
+              
                console.log(results);
-   
+
              results.data.forEach(function(r,index){
                  var imgDiv = $('<div>');
                  var img  = $('<img>');
                  var p = $('<p>');
                  var p2 = $('<p>');
                    imgDiv.attr('class','grid-item');
-                   img.attr('data-pic', index );
+                   //adding a image data-pic after buttons are updated//
+                   img.attr('data-pic', index);//
+                   img.attr('data-pic-number', (index + dataPicCount)-10 );
+                   img.attr('data-name', topic)
                    p.html("Rating: " + r.rating);
                    p2.html("Title: "+ r.title)
                    img.attr('class','images');
                    
                    img.attr('src', r.images.fixed_height_still.url ); 
-                  imgDiv.appendTo('#images');
+                    imgDiv.appendTo('#images');
                    img.appendTo(imgDiv);
                    p.appendTo(imgDiv);
                    p2.appendTo(imgDiv);
-                   
-   
-                   //click image to make giph move or still 
-               $('img').on('click', function(){
-                   var picNum = $(this).data('pic');
-                   var picAction = $(this).attr('src');
-                   
-                   if(picAction === results.data[picNum].images.fixed_height.url){
-                       $(this).attr('src', results.data[picNum].images.fixed_height_still.url); 
-                         }else{
-                       $(this).attr('src', results.data[picNum].images.fixed_height.url);
-                     }
-     
-               });
-           
-   
+                
              });
+
+         }); 
+       
+         dataPicCount +=10;
+       });
+
+        //click image to make giph move or still 
+        $('#images').on('click','img',function(){
+    
+          var search = $(this).data('name');
+          var picNum = $(this).data('pic');
+          var picAction = $(this).attr('src')
+          var picSelect = $(this).data('pic-number');
+
+         
+            var url = `https://api.giphy.com/v1/gifs/search?q=${search}&api_key=Jb3ttdnYHru0VwVp3pTkk1thutShY83T&limit=10`;
    
-      
-         });
-   
-         });
-   
+            $.ajax({
+             method: 'GET',
+             url: url,
+             
+             }).then(function(r){
+
+                console.log(r.data);
+             console.log($("[data-pic-number=" + picSelect + "]"));
+           
+               
+                
+                if(picAction === r.data[picNum].images.fixed_height.url){
+                  console.log('if');     
+                  $(this).attr('src',r.data[picNum].images.fixed_height_still.url);
+                  $("[data-pic-number=" + picSelect + "]").attr('src',r.data[picNum].images.fixed_height_still.url);
+                }else{
+                  console.log('else');
+                  console.log( $(this).attr('src'));
+                  
+                  $(this).attr('src',r.data[picNum].images.fixed_height.url );
+                  $("[data-pic-number=" + picSelect + "]").attr('src',r.data[picNum].images.fixed_height.url);
+                }
+    
+
+             });
+              
+                
+            
+
+
+
+     
+
+      });//end pic click
+
        });//end doc ready
